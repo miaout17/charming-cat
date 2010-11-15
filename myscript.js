05aa99cf83a6557ba9c617d0063443f6ee636ad8
gaagaaga <yzugsr@gmail.com>
2010-11-15 21:17:15 +0800

    init

---
+++ myscript.js
@@ -0,0 +1,10 @@
+$(".item").each( function() {
+	var item = $(this); 
+	var name = item.children(".name").text(); 
+	name = $.trim(name); 
+	if (!name)
+		return; 
+	var url = "http://progenitor-softworks.com/ew/index.php?title=" + name.replace(" ", "_"); 
+	var link = '<a href="' + url + '">[wiki]</a>';
+	item.children(".functions").append(link); 
+}); 
